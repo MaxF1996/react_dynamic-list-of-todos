44a30e2e8902c97a20dd7c2bb492caa2b9f6ef83
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-10-17 04:17:30 +0300

    search and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+// import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [currentFilter, setCurrentFilter] = useState('all');
+  const [currentSearch, setCurrentSearch] = useState('');
+
+  useEffect(() => {
+    getTodos().then(todosList => setTodos(todosList));
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    switch (currentFilter) {
+      case 'active':
+        return todos.filter(todo => !todo.completed);
+      case 'completed':
+        return todos.filter(todo => todo.completed);
+      default:
+        return todos;
+    }
+  }, [currentFilter, todos]);
+
+  const searchedTodos = useMemo(() => {
+    if (!currentSearch) {
+      return filteredTodos;
+    }
+
+    return filteredTodos.filter(todo =>
+      todo.title.toLowerCase().includes(currentSearch.toLowerCase()),
+    );
+  }, [currentSearch, filteredTodos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +48,21 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setFilter={setCurrentFilter}
+                setSearch={setCurrentSearch}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {filteredTodos.length === 0 && <Loader />}
+              <TodoList todos={searchedTodos} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {/* <TodoModal /> */}
     </>
   );
 };
